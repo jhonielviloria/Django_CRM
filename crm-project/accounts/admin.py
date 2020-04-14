97d50f00d523db503c284b950e2020fdbb5c1b45
Jhoniel Viloria <jhonielviloria@gmail.com>
2020-04-14 21:51:44 +0800

    added db models

--- crm-project/accounts/admin.py
+++ crm-project/accounts/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Product, Customer, Order
 
 # Register your models here.
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'created')
+    list_filter = ('name', 'created')
+    search_fields = ('name',)
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'phone')
+    search_fields = ('name', 'email', 'phone')
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('item', 'created', 'status')
+    search_fields = ('item', 'status')
--- crm-project/accounts/models.py
+++ crm-project/accounts/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.FloatField()
+    created = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.name
+
+class Customer(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    phone = models.CharField(max_length=20)
+    def __str__(self):
+        return self.name
+
+class Order(models.Model):
+    STATUS_CHOICES = (
+        ('Pending', 'pending'),
+        ('Ongoing', 'ongoing'),
+        ('Completed', 'completed'),
+    )
+    status = models.CharField(max_length=20,
+                              choices=STATUS_CHOICES,
+                              default='Pending')
+    item = models.ForeignKey(Product,
+                             on_delete=models.CASCADE,
+                             related_name='items')
+    customer = models.ForeignKey(Customer,
+                                 on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return f'{self.customer.name} ordered {self.item.name}'
